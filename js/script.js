3fea08d78fde59d655663d4c722b644eaf8a4b4c
alfahrelrifananda <95514134+alfahrelrifananda@users.noreply.github.com>
2024-09-02 15:16:09 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -95,5 +95,5 @@ const loadingScreen = document.querySelector('.loading-screen');
 
 window.addEventListener('load', function() {
   loadingScreen.style.display = 'none';
-  body.style.overflow = 'scroll';
+  body.style.overflowY = 'scroll';
 })
